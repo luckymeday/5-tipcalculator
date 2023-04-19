14afcf3c11b75222f898b2f17505384fe707dcff
jeesunlee <luckymeday@gmail.com>
2023-04-19 14:11:41 -0700

    working on js

---
+++ script.js
@@ -0,0 +1,43 @@
+function calculateTip() {
+  var inputBill = document.getElementById("inputBill").value;
+  console.log("input-bill :", inputBill);
+
+  var selectTip = document.getElementById("selectTip").value;
+  console.log("select-tip :", selectTip);
+
+  var customTip = document.getElementById("inputCustom").value / 100;
+  console.log("input-custom-tip :", customTip);
+
+  var numOfPeople = document.getElementById("inputPeople").value;
+  console.log("number of people :", numOfPeople);
+
+  if (numOfPeople === "") {
+    document.getElementById("alert").innerHTML = "Can't be zero";
+    return;
+  } else document.getElementById("alert").innerHTML = "";
+
+  var tipPerPerson = (inputBill * selectTip) / numOfPeople;
+  console.log("tip per person :", tipPerPerson);
+
+  document.getElementById("tipPerPerson").style.color = "white";
+  document.getElementById("tipPerPerson").innerHTML = tipPerPerson;
+
+  var totalPerPerson = (inputBill * (1 + selectTip)) / numOfPeople / 10;
+  console.log("total per person :", totalPerPerson);
+
+  document.getElementById("totalPerPerson").style.color = "white";
+  document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
+
+  //   var totalPerPerson = console.log("tip per person :", tipPerPerson);
+}
+
+function reset() {
+  console.log("reset buttom clicked?", reset);
+
+  document.getElementById("inputBill").value = "";
+  document.getElementById("selectTip").value = "";
+  document.getElementById("inputCustom").value = "";
+  document.getElementById("inputPeople").value = "";
+  document.getElementById("tipPerPerson").innerHTML = "$0.00";
+  document.getElementById("totalPerPerson").innerHTML = "$0.00";
+}
