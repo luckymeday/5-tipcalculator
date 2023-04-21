29e47d2d1863c5d7b183dc718f0ae15675562a34
jeesunlee <luckymeday@gmail.com>
2023-04-21 11:32:09 -0700

    error fixed

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
+var tip05 = 0,
+  tip10 = 0,
+  tip15 = 0,
+  tip25 = 0,
+  tip50 = 0;
+tipCustom = 0;
+var inputTip = 0;
+
+function selectTip05() {
+  tip05 = 0.05;
+  inputTip = tip05;
+  console.log("1.tip05", inputTip);
+}
+function selectTip10() {
+  tip10 = 0.1;
+  inputTip = tip10;
+  console.log("2.tip10", inputTip);
+}
+function selectTip15() {
+  tip15 = 0.15;
+  inputTip = tip15;
+  console.log("3.tip15", inputTip);
+}
+function selectTip25() {
+  tip25 = 0.25;
+  inputTip = tip25;
+  console.log("4.tip25", inputTip);
+}
+function selectTip50() {
+  tip50 = 0.5;
+  inputTip = tip50;
+  console.log("5.tip50", inputTip);
+}
+function customTip() {
+  tipCustom = document.getElementById("customTip").value / 100;
+  inputTip = tipCustom;
+  console.log("6.custom selected", inputTip);
+}
+
 function calculateTip() {
   var inputBill = document.getElementById("inputBill").value;
   console.log("input-bill :", inputBill);
 
-  var selectTip = document.getElementById("selectTip.value[]");
-  console.log("******select-tip :", selectTip);
-
-  var customTip = document.getElementById("inputCustom").value / 100;
-  console.log("input-custom-tip :", customTip);
-
   var numOfPeople = document.getElementById("inputPeople").value;
   console.log("number of people :", numOfPeople);
 
-  //   for (let i = 1; i <= 5; i++) {
-  //     let inputTip = document.getElementById("selectTip.value");
-  //     if (selectTip != null) inputTip = selectTip;
-  //     else inputTip = customTip;
-  //     console.log("inputTip :", selectTip);
-  //   }
+  if (inputTip === "") {
+    customTip = inputTip;
+  }
 
   if (numOfPeople === "") {
     document.getElementById("alert").innerHTML = "Can't be zero";
@@ -24,7 +54,7 @@ function calculateTip() {
     return;
   } else document.getElementById("alert").innerHTML = "";
 
-  var tipPerPerson = (inputBill * selectTip) / numOfPeople;
+  var tipPerPerson = (inputBill * inputTip) / numOfPeople;
   console.log("tip per person :", tipPerPerson);
 
   document.getElementById("tipPerPerson").style.color = "white";
